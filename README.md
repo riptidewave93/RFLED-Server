7f016010f11f72ffa2c2d43a11b7334d9af01016
riptidewave93 <chrisrblake93@gmail.com>
2014-09-27 13:41:23 -0500

    Initial Upload

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 RFLED-Server
 ============
+
+Python Scripts to run UDP servers to emulate a LimitlessLED WiFi Bridge 4.0 unit.
+
+Install
+=======
+
+ * Change the variables in both scripts to meet your needs
+ * Start the scripts and they will start the UDP listeners
---
+++ source/led.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import socket
+import serial
+
+# Set LED Control server settings
+UDP_IP = '' # Leave empty for Broadcast support
+LED_PORT = 8899
+
+# Serial Settings
+TTL_PORT = "/dev/ttyAMA0"
+TTL_SPEED = 9600
+
+# Create UDP socket, bind to it
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock.bind((UDP_IP, LED_PORT))
+
+while True:
+    data, addr = sock.recvfrom(64) # buffer size is 64 bytes
+
+    if data is not None:
+        # print("led command: ", str(data)) # Debugging
+        ser = serial.Serial(TTL_PORT, TTL_SPEED) # Connect to serial
+        ser.write(data) # Write packet data out
+
